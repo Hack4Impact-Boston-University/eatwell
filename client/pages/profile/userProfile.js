1814d91a5069eeb227d8bac9acae1aa9f73abe72
aaronli39 <aaronli39@gmail.com>
2020-10-08 21:47:16 -0400

    gitignore not working

--- client/pages/profile/userProfile.js
+++ client/pages/profile/userProfile.js
@@ -1,19 +1,28 @@
-import Head from 'next/head'
-import * as ui from '@material-ui/core';
+import Head from "next/head";
+import * as ui from "@material-ui/core";
 
+const useStyles = ui.makeStyles((theme) => ({
+	heading: {
+		color: "blue",
+	},
+	avatar: {
+		height: theme.spacing(13),
+		width: theme.spacing(13),
+	},
+}));
 
 export default function userAuth() {
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const classes = useStyles();
 
-        <h1>
-          User Profile Page
-        </h1>
-        
-    </div>
-  )
+	return (
+		<div>
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<h1 className={classes.heading}>User Profile Page</h1>
+			<ui.Avat
+		</div>
+	);
 }
