81180082375fe92b6a7109f47c5bbf5f918ce37e
fgpacini <fgpacini@yahoo.com>
2020-10-17 17:14:38 -0400

    jsons

--- utils/auth/initFirebase.js
+++ utils/auth/initFirebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
-import firebaseConfig from "./auth.json"
+import firebaseConfig from "../auth.json"
 
 export default function initFirebase() {
   if (!firebase.apps.length) {
