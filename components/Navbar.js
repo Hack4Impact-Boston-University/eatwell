b545aaeafd864d6c38eec4dc9ac4e4582aa3138d
aaronli39 <aaronli39@gmail.com>
2020-10-30 17:34:36 -0400

    added many form fields and logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,82 +1,92 @@
 import React from "react";
 import {
-	AppBar,
-	Avatar,
-	Box,
-	Button,
-	makeStyles,
-	Toolbar,
-	Typography,
+    AppBar,
+    Avatar,
+    Box,
+    Button,
+    makeStyles,
+    Toolbar,
+    Typography,
 } from "@material-ui/core";
 import { useRouter } from "next/router";
 
 const useStyles = makeStyles((theme) => ({
-	logo: {
-		marginRight: theme.spacing(2),
-	},
-	logoContainer: {
-		display: "flex",
-		"&:hover": {
-			cursor: "pointer",
-		},
-	},
-	toolbar: {
-		display: "flex",
-		justifyContent: "space-between",
-		background: "black",
-	},
-	centerText: {
-		display: "flex",
-		alignItems: "center",
-	},
-	menuContainer: {
-		display: "flex",
-		alignItems: "center",
-	},
-	menuItems: {
-		padding: "8px",
-		color: "white",
-	},
+    logo: {
+        marginRight: theme.spacing(2),
+    },
+    logoContainer: {
+        display: "flex",
+        "&:hover": {
+            cursor: "pointer",
+        },
+    },
+    toolbar: {
+        display: "flex",
+        justifyContent: "space-between",
+        background: "lightgreen",
+    },
+    centerText: {
+        display: "flex",
+        alignItems: "center",
+    },
+    menuContainer: {
+        display: "flex",
+        alignItems: "center",
+    },
+    menuItems: {
+        padding: "8px",
+        color: "black",
+    },
 }));
 
 const Navbar = () => {
-	const router = useRouter();
-	const classes = useStyles();
+    const router = useRouter();
+    const classes = useStyles();
 
-	return (
-		<div>
-			<AppBar position="static">
-				<Toolbar className={classes.toolbar}>
-					<Box
-						onClick={(e) => {
-							e.preventDefault;
-							router.push("/");
-						}}
-						className={classes.logoContainer}
-					>
-						<Avatar alt="Logo" src="L" className={classes.logo} />
-						<Box className={classes.centerText}>
-							<Typography variant="h6" style={{ userSelect: "none" }}>
-								EatWell
-							</Typography>
-						</Box>
-					</Box>
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar className={classes.toolbar}>
+                    <Box
+                        onClick={(e) => {
+                            e.preventDefault;
+                            router.push("/");
+                        }}
+                        className={classes.logoContainer}
+                    >
+                        <Avatar
+                            alt="Logo"
+                            src="/assets/eatwell_logo.png"
+                            className={classes.logo}
+                        />
+                        <Box className={classes.centerText}>
+                            <Typography
+                                variant="h6"
+                                style={{
+                                    userSelect: "none",
+                                    color: "black",
+                                }}
+                            >
+                                EatWell
+                            </Typography>
+                        </Box>
+                    </Box>
 
-					<Box className={classes.menuContainer}>
-						<Button href="/" className={classes.menuItems}>
-							Home
-						</Button>
-						<Button href="#" className={classes.menuItems}>
-							Recipes
-						</Button>
-						<Button href="/profile" className={classes.menuItems}>
-							Profile
-						</Button>
-					</Box>
-				</Toolbar>
-			</AppBar>
-		</div>
-	);
+                    <Box className={classes.menuContainer}>
+                        <Button href="/" className={classes.menuItems}>
+                            Home
+                        </Button>
+                        <Button href="#" className={classes.menuItems}>
+                            Recipes
+                        </Button>
+                        <Button href="/profile" className={classes.menuItems}>
+                            Profile
+                        </Button>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 };
 
 export default Navbar;
--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,23 @@
-import { AppBar, Avatar, Button, Toolbar, Typography, makeStyles } from "@material-ui/core";
+import {
+	AppBar,
+	Avatar,
+	Button,
+	Toolbar,
+	Typography,
+	makeStyles,
+	Grid,
+	FormControl,
+	InputLabel,
+	InputAdornment,
+	IconButton,
+	TextField,
+	OutlinedInput,
+} from "@material-ui/core";
 import { useUser } from "../utils/auth/useUser";
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
+import { Alert } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
 	heading: {
@@ -16,20 +33,63 @@ const useStyles = makeStyles((theme) => ({
 		display: "block",
 		margin: "auto",
 		textAlign: "center",
-		marginTop: "1rem"
+		marginTop: "1rem",
+	},
+	formItems: {
+		marginTop: theme.spacing(2),
+	},
+}));
+
+const useStyles = makeStyles((theme) => ({
+	heading: {
+		color: "red",
+	},
+	avatar: {
+		height: theme.spacing(13),
+		width: theme.spacing(13),
+		margin: "auto",
+	},
+	btn: {
+		width: "8rem",
+		display: "block",
+		margin: "auto",
+		textAlign: "center",
+		marginTop: "1rem",
 	},
 	formStyle: {
 		margin: "auto",
-		maxWidth: "20rem"
-	}
-
-
+		maxWidth: "20rem",
+	},
 }));
-
 
 const Profile = () => {
 	const { user, logout } = useUser();
+
+	const [errorAlert, setErrorAlert] = useState(false);
+	const [successAlert, setSuccessAlert] = useState(false);
+	const [profile, setProfile] = useState({});
 	const classes = useStyles();
+
+	const handleUpload = (e) => {
+		e.preventDefault();
+		console.log(e.target.files);
+		const re = /(?:\.([^.]+))?$/;
+		let ext = re.exec(e.target.files[0].name)[1];
+		console.log("ext");
+		if (ext !== "png" && ext !== "jpg" && ext !== "jpeg") {
+			console.log("Please upload a .png, .jpg, or .jpeg file");
+			setErrorAlert(true);
+		} else {
+			console.log("file uploaded");
+			setSuccessAlert(true);
+		}
+		console.log(successAlert);
+	};
+
+	const handleClickShowPassword = (e) => {
+		e.preventDefault();
+		setProfile({ ...profile, showPassword: !profile.showPassword });
+	};
 
 	if (!user) {
 		console.log("User not logged in.");
@@ -41,9 +101,12 @@ const Profile = () => {
 				>
 					<Toolbar disableGutters>
 						<Avatar
-							style={{ marginLeft: "8px", marginRight: "8px" }}
+							style={{
+								marginLeft: "8px",
+								marginRight: "8px",
+							}}
 							alt="Logo"
-							src="Logo"
+							src="/assets/eatwell_logo.png"
 						/>
 						<Typography variant="h6" style={{ flex: 1 }}>
 							EatWell
@@ -57,12 +120,11 @@ const Profile = () => {
 			</div>
 		);
 	}
-
 	return (
 		<div>
 			<Navbar />
 			<div>
-				<p>You're signed in. Email: {user.email}</p>
+				<p>You're signed in. Email: {user?.email}</p>
 				<p
 					style={{
 						display: "inline-block",
@@ -74,31 +136,124 @@ const Profile = () => {
 				>
 					Log out
 				</p>
-			
-			<Avatar
-				src="https://pbs.twimg.com/profile_images/988263662761775104/Bu1EDlWo.jpg"
-				alt="profile pic"
-				className={classes.avatar}
-			/>
-
-<Button
-  variant="contained"
-  component="label"
-  className={classes.btn}
->
-  Upload File
-  <input
-    type="file"
-    style={{ display: "none" }}
-  />
-</Button>
-<form className={classes.formStyle}>
-<p>First Name: </p>
-<p>Last Name: </p>
-<p>Email: </p>
-<p>Telephone: </p>
-</form>
 			</div>
+			<Grid justify="center" container>
+				<Grid xs={12} item>
+					<Avatar
+						src="https://pbs.twimg.com/profile_images/988263662761775104/Bu1EDlWo.jpg"
+						alt="profile pic"
+						className={classes.avatar}
+					/>
+				</Grid>
+				<Grid xs={8} md={4} item>
+					<Button variant="contained" component="label" className={classes.btn}>
+						Upload File
+						<input
+							type="file"
+							onChange={handleUpload}
+							style={{ display: "none" }}
+						/>
+					</Button>
+					{errorAlert ? (
+						<Alert
+							onClose={() => setErrorAlert(false)}
+							variant="filled"
+							severity="error"
+						>
+							Please upload a single image (.png, jpg, jpeg)
+						</Alert>
+					) : (
+						""
+					)}
+					{successAlert ? (
+						<Alert
+							onClose={() => setSuccessAlert(false)}
+							variant="filled"
+							severity="success"
+						>
+							File successfully uploaded!
+						</Alert>
+					) : (
+						""
+					)}
+				</Grid>
+			</Grid>
+			<Grid justify="center" className={classes.formItems} container>
+				<TextField
+					id="profileFirst"
+					label="First Name"
+					defaultValue="John"
+					InputProps={{
+						readOnly: true,
+					}}
+				/>
+			</Grid>
+			<Grid justify="center" className={classes.formItems} container>
+				<TextField
+					id="profileLast"
+					label="Last Name"
+					defaultValue="Doe"
+					InputProps={{
+						readOnly: true,
+					}}
+				/>
+			</Grid>
+			<Grid justify="center" className={classes.formItems} container>
+				<TextField
+					id="profileEmail"
+					label="Email"
+					defaultValue="abc@gmail.com"
+					InputProps={{
+						readOnly: true,
+					}}
+				/>
+			</Grid>
+			<Grid justify="center" className={classes.formItems} container>
+				<TextField
+					id="profilePhone"
+					label="Phone Number"
+					defaultValue="123-456-7890"
+					type="tel"
+					InputProps={{
+						readOnly: true,
+					}}
+				/>
+			</Grid>
+			<Grid justify="center" container>
+				<Grid xs={10} md={4} lg={2} className={classes.formItems} item>
+					<FormControl variant="outlined" margin="dense" fullWidth>
+						<InputLabel htmlFor="profilePassword">Password</InputLabel>
+						<OutlinedInput
+							id="profilePassword"
+							type={profile?.showPassword ? "text" : "password"}
+							// value={profile?.password}
+							value={"password"}
+							inputProps={{
+								readOnly: true,
+							}}
+							endAdornment={
+								<InputAdornment position="end">
+									<IconButton
+										aria-label="toggle password visibility"
+										onClick={handleClickShowPassword}
+										edge="end"
+									>
+										{profile?.showPassword ? <Visibility /> : <VisibilityOff />}
+									</IconButton>
+								</InputAdornment>
+							}
+							labelWidth={70}
+						/>
+					</FormControl>
+				</Grid>
+			</Grid>
+
+			{/* <form className={classes.formStyle}>
+                <p>First Name: </p>
+                <p>Last Name: </p>
+                <p>Email: </p>
+                <p>Telephone: </p>
+            </form> */}
 		</div>
 	);
 };
