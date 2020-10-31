63f63eea7139ed4e4f17a088003b66579a41c05a
zwan1312 <zwan1312@bu.edu>
2020-10-31 18:37:29 -0400

    pdf upload

--- pages/recipes/upload.js
+++ pages/recipes/upload.js
@@ -1,15 +1,24 @@
 import React, {useState} from 'react'
 import * as ui from '@material-ui/core'
-import * as firebase from 'firebase';
-import 'firebase/firestore';
-import initFirebase from '../../utils/auth/initFirebase';
+import * as firebase from 'firebase'
+import 'firebase/firestore'
+import initFirebase from '../../utils/auth/initFirebase'
+import { DropzoneArea } from 'material-ui-dropzone'
+import { PictureAsPdf } from '@material-ui/icons'
 
 initFirebase()
+
+const handlePreviewIcon = (fileObject, classes) => {
+    const iconProps = {
+      className : classes.image,
+    }
+    return <PictureAsPdf {...iconProps} />
+}
 
 const UploadForm = () => {
     const [recipeName, setRecipeName] = useState('')
     const [videoID, setVideoID] = useState('')
-    const [pdfUrl, setPdfUrl] = useState('')
+    const [pdfFile, setPdfFile] = useState('')
 
     function upload() {
         const videoUrl = "https://player.vimeo.com/video/" + videoID
@@ -19,13 +28,13 @@ const UploadForm = () => {
     
         var collection = firebase.firestore().collection('recipes')
         var data = {
-            pdfUrl: pdfUrl,
-            videoUrl: videoUrl
+            videoUrl: videoUrl,
         }
         
         collection.doc(recipe).set(data)
+        firebase.storage().ref().child(recipeName+".pdf").put(pdfFile)
 
-        alert('upload successful!')
+        alert("Your upload was successful!")
     }
 
     return (
@@ -35,35 +44,38 @@ const UploadForm = () => {
         </ui.Typography>
         <ui.Grid container spacing={3}>
             <ui.Grid item xs={12} sm={6}>
-            <ui.TextField
-                required
-                value={recipeName}
-                label="Recipe Name"
-                onChange={(e) => setRecipeName(e.target.value)}
-                fullWidth
-            />
+                <ui.TextField
+                    required
+                    value={recipeName}
+                    label="Recipe Name"
+                    onChange={(e) => setRecipeName(e.target.value)}
+                    fullWidth
+                />
             </ui.Grid>
             <ui.Grid item xs={12} sm={6}>
-            <ui.TextField
-                required
-                value={videoID}
-                label="Vimeo Video ID"
-                onChange={(e) => setVideoID(e.target.value)}
-                fullWidth
-            />
+                <ui.TextField
+                    required
+                    value={videoID}
+                    label="Vimeo Video ID"
+                    onChange={(e) => setVideoID(e.target.value)}
+                    fullWidth
+                />
             </ui.Grid>
             <ui.Grid item xs={12}>
-            <ui.TextField
-                required
-                value={pdfUrl}
-                label="URL of PDF File"
-                onChange={(e) => setPdfUrl(e.target.value)}
-                fullWidth
-            />
+                <DropzoneArea
+                    accept="application/pdf"
+                    maxFileSize={10485760}
+                    dropzoneText="Click to select or drag and drop recipe PDF"
+                    filesLimit={1}
+                    getPreviewIcon={handlePreviewIcon}
+                    onChange={(files) => setPdfFile(files[0])}
+                />
             </ui.Grid>
-            <ui.Button variant="outlined">
-            <a onClick={() => upload()}>Upload</a>
-            </ui.Button>
+            <ui.Grid item xs={12}>
+                <ui.Button variant="outlined" fullWidth>
+                    <a onClick={() => upload()}>Upload</a>
+                </ui.Button>
+            </ui.Grid>
 
         </ui.Grid>
         </React.Fragment>
