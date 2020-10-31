7c98a0a75ae5635aa699b3b672f5b060071b6e54
iccchen <iccchen@bu.edu>
2020-10-31 13:08:50 -0400

    toggle button ui

--- pages/recipes/[recipeName].js
+++ pages/recipes/[recipeName].js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import * as ui from '@material-ui/core';
+import * as toggle from '@material-ui/lab';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import BuildIcon from '@material-ui/icons/Build';
+import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
+
 
 const fetcher = async (...args) => {
   const res = await fetch(...args);
@@ -44,6 +49,7 @@ function useWindowSize() {
   return windowSize;
 }
 
+
 export default function Recipe() {
   const router = useRouter();
   const { recipeName } = router.query;
@@ -53,6 +59,12 @@ export default function Recipe() {
   if (!data) {
     return 'Loading...';
   }
+
+  const [value, setValue] = React.useState(2);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <div>
@@ -69,7 +81,36 @@ export default function Recipe() {
         </ui.Toolbar>
       </ui.AppBar>
 
-      <iframe src={data.videoUrl} width="100%" height={(width*0.625)} frameBorder="0" align="center" position="sticky" allow="autoplay; fullscreen"></iframe>
+      
+      {/* <ui.Paper square>
+        <ui.Tabs
+          // value={value}
+          indicatorColor="primary"
+          textColor="primary"
+          // onChange={handleChange}
+          aria-label="disabled tabs example"
+        >
+          <AssignmentIcon/><ui.Tab label="Recipe" />
+          <BuildIcon/><ui.Tab label="Skill"/>
+          <EmojiObjectsIcon/><ui.Tab label="Tip" />
+        </ui.Tabs>
+      </ui.Paper> */}
+
+      <toggle.ToggleButtonGroup aria-label="text formatting">
+        <toggle.ToggleButton value="bold" aria-label="bold">
+          <AssignmentIcon/><text>Recipe</text>
+        </toggle.ToggleButton>
+        <toggle.ToggleButton value="italic" aria-label="italic">
+          <BuildIcon/><text>Skill</text>
+        </toggle.ToggleButton>
+        <toggle.ToggleButton value="underlined" aria-label="underlined">
+          <EmojiObjectsIcon/><text>Tip</text>
+        </toggle.ToggleButton>
+      </toggle.ToggleButtonGroup>
+
+      <div id="recipe">
+        <iframe src={data.videoUrl} width="100%" height={(width*0.625)} frameBorder="0" align="center" position="sticky" allow="autoplay; fullscreen"></iframe>
+      </div>
     </div>
   );
 }
