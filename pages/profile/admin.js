fca2714b07783b1ff7774da0e035f4b8181c3ea9
iccchen <iccchen@bu.edu>
2020-11-11 11:35:33 -0500

    admin page

---
+++ pages/profile/admin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+
+
+const Admin = () => {
+    return (
+        <TextField id="outlined-basic" label="Enter Email" variant="outlined" />
+        
+
+    )
+}
+export default Admin
