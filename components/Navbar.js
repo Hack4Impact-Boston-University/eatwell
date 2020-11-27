ad26f636e5e3579867616602bc82afe87694440e
aaronli39 <aaronli39@gmail.com>
2020-11-27 15:33:54 -0500

    fixed profile icons centering

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,30 +4,37 @@ import {
 	Avatar,
 	Box,
 	Button,
+	Grid,
+	IconButton,
 	makeStyles,
 	Toolbar,
 	Typography,
 } from "@material-ui/core";
 import { useUser } from "../utils/auth/useUser";
 import Link from "next/link";
-import { AccountCircle, MenuBook } from "@material-ui/icons";
+import {
+	AccountCircle,
+	Book,
+	ExitToApp,
+	KeyboardArrowRight,
+} from "@material-ui/icons";
 
-const drawerWidth = 100
-const barWidth = 60
+const drawerWidth = 100;
+const barWidth = 60;
 
 const useStyles = makeStyles((theme) => ({
 	root: {
-		display: 'flex',
+		display: "flex",
 	},
 	appBar: {
-		[theme.breakpoints.up('sm')]: {
+		[theme.breakpoints.up("sm")]: {
 			width: `calc(100% - ${drawerWidth}px)`,
 			marginLeft: drawerWidth,
 		},
 	},
 	menuButton: {
-		[theme.breakpoints.up('sm')]: {
-			display: 'none',
+		[theme.breakpoints.up("sm")]: {
+			display: "none",
 		},
 	},
 	logo: {
@@ -45,24 +52,18 @@ const useStyles = makeStyles((theme) => ({
 		display: "flex",
 		justifyContent: "space-between",
 		background: "#0A5429",
-		height: barWidth
+		height: barWidth,
 	},
 	centerText: {
 		display: "flex",
 		alignItems: "center",
 	},
-	menuContainer: {
-		display: "flex",
-	},
 	menuItems: {
-		display: "flex",
-		alignItems: "center",
-		// padding: "4px",
 		color: "#EEF8F9",
 	},
 	menuIcon: {
-		marginRight: theme.spacing(0.5)
-	}
+		marginRight: theme.spacing(0.2),
+	},
 }));
 
 const Navbar = () => {
@@ -75,48 +76,53 @@ const Navbar = () => {
 		return (
 			<div>
 				{user ? (
-					<Box className={classes.menuContainer}>
+					// user logged in show menu items
+					<Toolbar disableGutters>
 						<Link href="/recipes/chicken_fried_rice">
-							<Button>
-								<Box component="div" className={classes.menuItems}>
-									<MenuBook className={classes.menuIcon} />
-									<Typography variant="subtitle2">Recipes</Typography>
-								</Box>
+							<Button className={classes.menuItems}>
+								<Book />
+								<Typography variant="subtitle2">Recipes</Typography>
 							</Button>
 						</Link>
+
 						<Link href={`/profile/profile`}>
-							<Button>
-								<Box component="div" className={classes.menuItems}>
-									<AccountCircle className={classes.menuIcon} />
-									<Typography variant="subtitle2">Profile</Typography>
-								</Box>
+							<Button className={classes.menuItems}>
+								<AccountCircle
+									className={`${classes.menuIcon} ${classes.menuItems}`}
+								/>
+								<Typography variant="subtitle2">Profile</Typography>
 							</Button>
 						</Link>
-						<Button onClick={() => logout()} className={classes.menuItems}>
+
+						<Button
+							onClick={() => logout()}
+							className={`${classes.menuIcon} ${classes.menuItems}`}
+						>
+							<ExitToApp />
 							<Typography variant="subtitle2">Logout</Typography>
 						</Button>
-					</Box>
-				) : ( // user not logged in show login button
+					</Toolbar>
+				) : (
+					// user not logged in show login button
+					<Link href={`/login`}>
 						<Button className={classes.menuItems}>
-							<Link href={`/login`}>
-								<a>Login</a>
-							</Link>
+							<KeyboardArrowRight
+								className={`${classes.menuIcon} ${classes.menuItems}`}
+							/>
+							<Typography variant="subtitle1">Login</Typography>
 						</Button>
-					)
-				}
-			</div >
+					</Link>
+				)}
+			</div>
 		);
-	}
+	};
 
 	return (
 		<div>
 			<div>
 				<AppBar position="static">
 					<Toolbar className={classes.toolbar}>
-						<Button
-							href="/"
-							className={classes.logoContainer}
-						>
+						<Button href="/" className={classes.logoContainer}>
 							<Avatar
 								alt="Logo"
 								src="/assets/eatwell_logo.png"
