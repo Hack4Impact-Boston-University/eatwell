5cb2e8a5f0a582afcc76973a4f6fcbc7b0d0437f
fgpacini <fgpacini@yahoo.com>
2021-01-08 19:19:52 -0500

    Responsive recipes

--- pages/recipes/recipeCard.js
+++ pages/recipes/recipeCard.js
@@ -13,6 +13,8 @@ import {
   editRatingsCookie,
 } from "../../utils/cookies";
 import ClearIcon from '@material-ui/icons/Clear';
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 import {uploadRating, getRecipe, setRecipeListener} from "../../utils/recipes.js";
 import firebase from "firebase/app";
 import "firebase/auth";
@@ -28,12 +30,10 @@ const useStyles = makeStyles((theme) => ({
       textAlign: "center",
 	  },
     card: {
-      width: "100%",
       boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
       "&:hover": {
         boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
       }
-      //maxWidth:500,
     },
   
     media: {
@@ -53,7 +53,21 @@ const useStyles = makeStyles((theme) => ({
       width: 100
     },
     formItems: {
-      marginTop: theme.spacing(2),
+      marginTop: theme.spacing(0),
+    },
+    icon: {
+      fontSize: "calc(max(2vw,17px))"
+    },
+    iconContainer: {
+      // width: "3vw",
+      // height: "3vw",
+    },
+    viewButtonLabel: { textTransform: "none" },
+    text: {
+      fontSize: 'calc(min(2.7vw, 17px))'
+    },
+    label: {
+      fontSize: 'calc(min(3vw, 20px))'
     }
   }));
 
@@ -139,141 +153,271 @@ export default function RecipeCard({ object, isFav, onFavClick, initNotes, initR
     editRatingsCookie(obj.id, val);
   }
 
-  console.log(obj.images)
-
-
   return (
-    <Grid item xs={12} >
-      <Box paddingBottom={5}>
-      <Card className={classes.card}>
-
-        <CardContent>
-          <Grid container spacing={5} >
-            <Grid item xs={1} >
-              <IconButton 
-                onClick = {favButtonClick}
-                aria-label = "add to favorites" 
-                color= {favorited? "secondary" : "default"}>
-                <FavoriteIcon />
-              </IconButton>
-            </Grid>
-
-            <Grid item xs={7} >
-              <Link href={obj.id}>
-                <Typography style={{ fontSize: 35, fontWeight: 300 }}  >
-                  {obj.nameOfDish}
-                </Typography>
-              </Link>
-            </Grid>
-
-            <Grid item xs={2} spacing={0} style={{ paddingTop: 35 }}>
-              <Typography style={{ fontSize: 20, fontWeight: 300 }}  >
-                Date: {obj.dateUploaded}
-              </Typography>
-            </Grid>
-
-            <Grid item xs={2} style={{ paddingTop: 35 }}>
-              <Typography style={{ fontSize: 20, fontWeight: 300 }}  >
-                Average Rating: {obj.avgRating}
-              </Typography>
-              <Rating
-                  defaultValue={0}
-                  precision={0.5}
-                  onChange={(e) => {changeRating(e.target.value)}}
-                  value={rating}
-              />
-              {rating > 0 && <ClearIcon onClick={() => {changeRating(0)}} />}
-              {obj.numRatings}
-            </Grid>
-
-            <Grid item xs={12} >
-              <ui.Divider light />
-            </Grid>
-
-            <Grid item xs={6} >
+    <Grid item xs={12}>
+      <Box pb={3}>
+        <Card className={classes.card}>
+          <CardContent>
+            <Box m={"0.25vw"}> 
+              <Grid container>
+                <Grid item xs={2} sm={1}>
+                  <IconButton 
+                    onClick = {favButtonClick}
+                    aria-label = "add to favorites" 
+                    color= {favorited? "secondary" : "default"}
+                    className={classes.iconContainer}
+                  >
+                    <FavoriteIcon className={classes.icon}/>
+                  </IconButton>
+                  
+                </Grid>
+                <Grid container item xs={10} alignItems="center" justify="center">
+                      <Link href={obj.id}>
+                        <Typography style={{ fontSize: "calc(min(5vw, 35px))", fontWeight: 300 }}  >
+                          {obj.nameOfDish}
+                        </Typography>
+                      </Link> 
+                </Grid>
+              </Grid>
+              <Grid container justify="center">
+                {
+                  (obj.images == undefined) ? 
+                  <Grid item xs={12} >
+                  </Grid> :
+                  <Grid item xs={9} >
+                      <img className={classes.media} src={obj.images[0]} />
+                  </Grid>
+                }
+              </Grid>
+              <Grid container item xs={12} justify="center">
+                <Button variant="contained" color="secondary" classes={{label: classes.viewButtonLabel}}>
+                  <Link href={obj.id}>
+                     Make this Recipe
+                  </Link>
+                </Button>
+              </Grid>
+              <Grid container justify="center" spacing={9} style={{ paddingTop: 10 }}>
+                <Grid item xs={2}>
+                  <Typography style={{ fontSize: "calc(min(3vw, 20px))", fontWeight: 300 }}  >
+                    Date: {obj.dateUploaded}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                  <Typography style={{ fontSize: "calc(min(3vw, 20px))", fontWeight: 300 }}  >
+                    Average: {obj.avgRating} / 5
+                  </Typography>
+                  <Rating
+                    defaultValue={0}
+                    precision={0.5}
+                    onChange={(e) => {changeRating(e.target.value)}}
+                    value={rating}
+                    style={{ fontSize: "calc(min(3vw, 20px))"}}
+                  />
+                  {rating > 0 && <ClearIcon onClick={() => {changeRating(0)}} style={{ fontSize: "calc(min(3vw, 20px))"}}/> }
+                  <Typography style={{ fontSize: "calc(min(3vw, 20px))", fontWeight: 300 }}  >
+                    {obj.numRatings} Rating{obj.numRatings > 1 ? "s" : ""}
+                  </Typography>
+                </Grid>
+              </Grid>
               <Grid container spacing={3}  >
                 <Grid item xs={12} >
-                  <Typography style={{ fontSize: 20, fontWeight: 300 }}  >
+                  <Typography style={{ fontSize: "calc(min(3vw, 20px))", fontWeight: 300 }}  >
                     {obj.description}
                   </Typography>
                 </Grid>
-                <Grid item xs={12} >
-                  <Button variant="contained" color="secondary">
-                    <Link href={obj.id}>
-                      Make this Recipe
-                    </Link>
-                  </Button>
-                </Grid>
                 <Grid item xs={6} >
-                  <Typography style={{ fontSize: 18, fontWeight: 300 }}  >
+                  <Typography style={{ fontSize: "calc(min(2.7vw, 18px))", fontWeight: 300 }}  >
                     Skills:
                   </Typography>
                 </Grid>
-
               </Grid>
-            </Grid>
-
-            {
-              (obj.images == undefined) ? 
-              <Grid item xs={6} >
-              </Grid> :
-              <Grid item xs={6} >
-              <ui.Box>
-                <img className={classes.media} src={obj.images[0]} />
-              </ui.Box>
-            </Grid>
-            }
-          </Grid>
-
-        </CardContent>
-
-        <CardActions disableSpacing>
-          <Grid container spacing={0} direction="row" alignItems="center" justify="center">
-            <Typography style={{ fontSize: 18, fontWeight: 300 }}  >
-                Notes
-            </Typography>
-		      </Grid>
-          
-            
-          <IconButton
-            className={clsx(classes.expand, {
-              [classes.expandOpen]: expanded,
-            })}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </IconButton>
-        </CardActions>
-
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          
-          <Grid container spacing={0} direction="column" alignItems="center" justify="center" style={{ minHeight: '10vh' }}>
-            <Grid justify="center" direction="row" className={classes.formItems} container>
-              <TextField
-                value={note}
-                onChange={(e) => setNote(e.target.value)}
-                label="Note"
-                placeholder="Add a Note"
-              />
-              <Button color="primary" className={classes.btn} style={{marginTop: "1rem"}} onClick={() => handleSubmit()}>
-                  Submit
-              </Button>
-            </Grid>
-            <Box m={5}>
-              {
-                notes.map((str, idx) => {
-                  return (<Note str={str} setStr={(s) => setStr(s, idx)} deleteStr={() => deleteStr(idx)}> </Note>);         
-                })
-              }
             </Box>
-		      </Grid>
-        </Collapse>
-      </Card>
+          </CardContent>
+          <CardActions disableSpacing>
+            <Grid container direction="row" alignItems="center" justify="center">
+              <Typography style={{ fontSize: "calc(min(2.7vw, 18px))", fontWeight: 300 }}  >
+                  Notes
+              </Typography>
+            </Grid>
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <Grid container direction="column" alignItems="center" justify="center">
+              <Grid justify="center" direction="row" className={classes.formItems} container>
+                <TextField
+                  value={note}
+                  onChange={(e) => setNote(e.target.value)}
+                  label="Note"
+                  placeholder="Add a Note"
+                  InputProps={{
+                    classes: {input: classes.text},
+                  }}
+                  InputLabelProps={{
+                    classes: {root: classes.label}
+                  }}
+                />
+                <Button color="primary" className={classes.btn} style={{marginTop: "1rem"}} onClick={() => handleSubmit()}>
+                  <Typography style={{ fontSize: "calc(min(2.7vw, 17px))", fontWeight: 1000 }}  >
+                    Submit
+                  </Typography>
+                </Button>
+              </Grid>
+              <Box m={"3vh"}>
+                {
+                  notes.map((str, idx) => {
+                    return (<Note str={str} setStr={(s) => setStr(s, idx)} deleteStr={() => deleteStr(idx)}> </Note>);         
+                  })
+                }
+              </Box>
+            </Grid>
+          </Collapse>
+        </Card>
       </Box>
     </Grid>
-    )
+  )
+
+  // return (
+  //   <Grid item xs={12} >
+  //     <Box paddingBottom={5}>
+  //     <Card className={classes.card}>
+
+  //       <CardContent>
+  //         <Grid container>
+  //           <Grid item xs={1}>
+  //             <IconButton 
+  //               onClick = {favButtonClick}
+  //               aria-label = "add to favorites" 
+  //               color= {favorited? "secondary" : "default"}
+  //               className={classes.iconContainer}
+  //             >
+  //               <FavoriteIcon className={classes.icon}/>
+  //             </IconButton>
+  //           </Grid>
+  //           <Grid item xs={10} >
+  //             <Link href={obj.id}>
+  //               <Typography style={{ fontSize: "calc(min(5vw, 35px))", fontWeight: 300 }}  >
+  //                 {obj.nameOfDish}
+  //               </Typography>
+  //             </Link>
+  //           </Grid>
+  //           {
+  //             (obj.images == undefined) ? 
+  //             <Grid item xs={6} >
+  //             </Grid> :
+  //             <Grid item xs={6} >
+  //             <ui.Box>
+  //               <img className={classes.media} src={obj.images[0]} />
+  //             </ui.Box>
+  //           </Grid>
+  //           }
+
+
+  //           <Grid item xs={2} spacing={0} style={{ paddingTop: 35 }}>
+  //             <Typography style={{ fontSize: 20, fontWeight: 300 }}  >
+  //               Date: {obj.dateUploaded}
+  //             </Typography>
+  //           </Grid>
+
+  //           <Grid item xs={2} style={{ paddingTop: 35 }}>
+  //             <Typography style={{ fontSize: 20, fontWeight: 300 }}  >
+  //               Average Rating: {obj.avgRating}
+  //             </Typography>
+  //             <Rating
+  //                 defaultValue={0}
+  //                 precision={0.5}
+  //                 onChange={(e) => {changeRating(e.target.value)}}
+  //                 value={rating}
+  //             />
+  //             {rating > 0 && <ClearIcon onClick={() => {changeRating(0)}} />}
+  //             {obj.numRatings}
+  //           </Grid>
+
+  //           <Grid item xs={12} >
+  //             <ui.Divider light />
+  //           </Grid>
+
+  //           <Grid item xs={6} >
+  //             <Grid container spacing={3}  >
+  //               <Grid item xs={12} >
+  //                 <Typography style={{ fontSize: 20, fontWeight: 300 }}  >
+  //                   {obj.description}
+  //                 </Typography>
+  //               </Grid>
+  //               <Grid item xs={12} >
+  //                 <Button variant="contained" color="secondary">
+  //                   <Link href={obj.id}>
+  //                     Make this Recipe
+  //                   </Link>
+  //                 </Button>
+  //               </Grid>
+  //               <Grid item xs={6} >
+  //                 <Typography style={{ fontSize: 18, fontWeight: 300 }}  >
+  //                   Skills:
+  //                 </Typography>
+  //               </Grid>
+
+  //             </Grid>
+  //           </Grid>
+  //         </Grid>
+
+  //       </CardContent>
+
+        // <CardActions disableSpacing>
+        //   <Grid container spacing={0} direction="row" alignItems="center" justify="center">
+        //     <Typography style={{ fontSize: 18, fontWeight: 300 }}  >
+        //         Notes
+        //     </Typography>
+		    //   </Grid>
+          
+            
+        //   <IconButton
+        //     className={clsx(classes.expand, {
+        //       [classes.expandOpen]: expanded,
+        //     })}
+        //     onClick={handleExpandClick}
+        //     aria-expanded={expanded}
+        //     aria-label="show more"
+        //   >
+        //     <ExpandMoreIcon />
+        //   </IconButton>
+        // </CardActions>
+
+        // <Collapse in={expanded} timeout="auto" unmountOnExit>
+          
+        //   <Grid container spacing={0} direction="column" alignItems="center" justify="center" style={{ minHeight: '10vh' }}>
+        //     <Grid justify="center" direction="row" className={classes.formItems} container>
+        //       <TextField
+        //         value={note}
+        //         onChange={(e) => setNote(e.target.value)}
+        //         label="Note"
+        //         placeholder="Add a Note"
+        //       />
+        //       <Button color="primary" className={classes.btn} style={{marginTop: "1rem"}} onClick={() => handleSubmit()}>
+        //           Submit
+        //       </Button>
+        //     </Grid>
+        //     <Box m={5}>
+        //       {
+        //         notes.map((str, idx) => {
+        //           return (<Note str={str} setStr={(s) => setStr(s, idx)} deleteStr={() => deleteStr(idx)}> </Note>);         
+        //         })
+        //       }
+        //     </Box>
+		    //   </Grid>
+        // </Collapse>
+  //     </Card>
+  //     </Box>
+  //   </Grid>
+  //   )
 };
 
 const Note = ({str, setStr, deleteStr}) => {
@@ -290,26 +434,40 @@ const Note = ({str, setStr, deleteStr}) => {
           onChange={(e) => setVal(e.target.value)}
           label="Note"
           placeholder="Add a Note"
+          InputProps={{
+            classes: {input: classes.text, label: classes.label},
+          }}
+          InputLabelProps={{
+            classes: {root: classes.label}
+          }}
           />
         <Button color="primary" className={classes.btn} onClick={() => {setEditing(false); setStr(val)}}>
-            Submit
-        </Button>
+          <Typography style={{ fontSize: "calc(min(2.7vw, 17px))", fontWeight: 1000 }}  >
+              Submit
+          </Typography>
+          </Button>
         <Button color="primary" className={classes.btn} onClick={() => setEditing(false)}>
-            Cancel
+          <Typography style={{ fontSize: "calc(min(2.7vw, 17px))", fontWeight: 1000 }}  >
+              Cancel
+          </Typography>
         </Button>
         </Grid> 
       ) : (
       <Grid justify="center" direction="row" className={classes.formItems} container>
         <Grid justify="center" style={{marginRight: "1rem", marginTop: "0.3rem", maxWidth: '60vw'}}>
-          <Typography>
-            {str}
+          <Typography style={{ fontSize: "calc(min(2.7vw, 17px))", fontWeight: 300 }}>
+            {str} 
           </Typography>
         </Grid>
         <Button color="primary" className={classes.btn} onClick={() => setEditing(true)}>
-            Edit
+          <Typography style={{ fontSize: "calc(min(2.7vw, 17px))", fontWeight: 1000 }}  >
+              Edit
+          </Typography>
         </Button>
         <Button color="primary" className={classes.btn} onClick={() => deleteStr()}>
-            Delete
+          <Typography style={{ fontSize: "calc(min(2.7vw, 17px))", fontWeight: 1000 }}  >
+              Delete
+          </Typography>
         </Button>
       </Grid>)
       }
