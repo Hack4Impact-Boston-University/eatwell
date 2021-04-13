c82d9bff4766a386bd1e4929627e77324ad4c1b3
aaronli39 <aaronli39@gmail.com>
2021-04-13 18:13:27 -0400

    favorites

---
+++ src/pages/profile/favorites.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+const favorites = () => {
+	return (
+		<div>
+			<Navbar />
+			<h2>Hello</h2>
+		</div>
+	);
+};
+
+export default favorites;
