7ad8ec5e5a71291a39d0570afa745a0064d4f8b5
Cici Chen <iccchen@bu.edu>
2021-06-26 04:36:35 +0800

    firebase analytics

--- src/utils/auth/initFirebase.js
+++ src/utils/auth/initFirebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/analytics'
 
 var firebaseConfig = {
   apiKey: "AIzaSyC2_aOpnms38eo6G2CmEwfIHMA04utDnis",
@@ -7,11 +8,13 @@ var firebaseConfig = {
   projectId: "eatwell-87d0c",
   storageBucket: "eatwell-87d0c.appspot.com",
   messagingSenderId: "270259249884",
-  appId: "1:270259249884:web:799489f9bda4607144ca19"
+  appId: "1:270259249884:web:799489f9bda4607144ca19",
+  measurementId: "G-GE5Q40JYR2"
 };
 
 export default function initFirebase() {
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig)
+    firebase.analytics();
   }
 }
